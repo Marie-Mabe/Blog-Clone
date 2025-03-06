45ce2e26931a9ed51e96454471d6abffc8387a6b
Mabé <marieaudrey@gmail.com>
2025-03-06 01:08:25 +0000

    Fix Link issue in not-found.tsx

--- app/posts/[slug]/not-found.tsx
+++ app/posts/[slug]/not-found.tsx
@@ -1,16 +1,17 @@
+import Link from "next/link";
+
 export default async function RoutePage() {
-    return (
-      <div className="flex  flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-destructive">
-          404 - Page Not Found
-        </h1>
-        <p className="mt-2">
-          Retourner à{" "}
-          <a href="/" className="text-primary hover:underline">
-            l'accueil
-          </a>
-          .
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-destructive">
+        404 - Page Not Found
+      </h1>
+      <p className="mt-2">
+        Retourner à{" "}
+        <Link href="/" className="text-primary hover:underline">
+          l'accueil
+        </Link>
+      </p>
+    </div>
+  );
+}
